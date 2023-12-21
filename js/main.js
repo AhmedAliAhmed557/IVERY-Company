61412a1d9ce5249e8a6d4ec4657dbcde2e8c6493
Ahmed Ali Ahmed <106572516+AhmedAliAhmed557@users.noreply.github.com>
2023-12-21 02:02:43 +0200

    uploading

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let toggleList = document.querySelector('.toggle-list')
+let ul = document.querySelector("nav ul")
+let closeBtn = document.querySelector(".close")
+
+// function toggleElement() {
+    toggleList.addEventListener("click" , () => {
+        if (ul.classList.contains("hidden")) {
+            ul.classList.remove("hidden")
+            ul.classList.add("visible")
+        } else {
+            ul.classList.remove("visible")
+            ul.classList.add("hidden")
+        }
+    })
+// }
+ 
+toggleList.addEventListener("click" , () => {
+    if (closeBtn.classList.contains("hide")) {
+        closeBtn.classList.remove("hide")
+        closeBtn.classList.add("visible")
+    } else {
+        closeBtn.classList.remove("visible")
+        closeBtn.classList.add("hide")
+    }
+})
+
+closeBtn.addEventListener("click", () => {
+    ul.classList.add("hidden")
+    closeBtn.classList.add("hidden")
+})
